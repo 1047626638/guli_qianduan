8eb29754f7c5205a0388808b2ebbc79d72423645
李鹏 <jr_lipeng@yonyou.com>
2022-11-02 17:03:03 +0800

    讲师模块增删改查

--- vue-admin-template-master/src/api/edu/teacher.js
+++ vue-admin-template-master/src/api/edu/teacher.js
@@ -18,5 +18,28 @@ export default{
           url:`/eduservice/edu-teacher/${id}`,
           method: 'delete'
         })
-    }
+    },
+    //添加讲师
+    addTeacher(teacher){
+        return request({
+          url:`/eduservice/edu-teacher/addTeacher`,
+          method: 'post',
+          data: teacher
+        })
+   },
+    //根据id查询讲师
+    getTeacherInfo(id){
+      return request({
+        url:`/eduservice/edu-teacher/getTeacher/${id}`,
+        method: 'get'
+      })
+   },
+    //修改讲师
+    updateTeacherInfo(teacher){
+    return request({
+      url:`/eduservice/edu-teacher/updateTeacher`,
+      method: 'post',
+      data: teacher
+    })
+  }
 }
